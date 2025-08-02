105ea34d4a5718f1138d78d41cb75ebe48cf3598
RyanOrtiz0246 <ryan142310@gmail.com>
2025-08-01 18:27:55 -0600

    Se creo lo correspondiente de Sales en los proyectos Application e Infrastructure

---
+++ SalesAnalysisPlatform.Infrastructure/Repositories/SaleRepository.cs
@@ -0,0 +1,50 @@
+﻿using SalesAnalysisPlatform.Domain.Entities;
+using SalesAnalysisPlatform.Infrastructure.Interfaces;
+using SalesAnalysisPlatform.Infrastructure.Core;
+using SalesAnalysisPlatform.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesAnalysisPlatform.Infrastructure.Repositories
+{
+    public class SaleRepository : BaseRepository<Sale>, ISaleRepository
+    {
+        private readonly SalesDbContext _context;
+
+        public SaleRepository(SalesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Sale>> GetAllSalesAsync()
+        {
+            return await _context.Sales.ToListAsync();
+        }
+
+        public async Task<Sale> GetSaleByIdAsync(int id)
+        {
+            return await _context.Sales.FindAsync(id);
+        }
+
+        public async Task AddSaleAsync(Sale sale)
+        {
+            await _context.Sales.AddAsync(sale);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateSaleAsync(Sale sale)
+        {
+            _context.Sales.Update(sale);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSaleAsync(int id)
+        {
+            var sale = await _context.Sales.FindAsync(id);
+            if (sale != null)
+            {
+                _context.Sales.Remove(sale);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
